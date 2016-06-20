be65c9331c5917a457997aa5cddb9a48b7e7830e
Guikingone <guillaume.loulier@hotmail.fr>
2016-06-20 18:39:21 +0200

    [!C] || Mise en place assets|En cours

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var minify_css = require('gulp-minify-css');
+
+var config = {
+    assets_dir : 'web/assets/sass',
+    sass_dir :  'Home/*.scss'
+};
+
+gulp.task('sass', function () {
+    gulp.src(config.assets_dir + '/' + config.sass_dir)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat('main.css'))
+        .pipe(minify_css())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('web/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.assets_dir + '/' + config.sass_dir, ['sass'])
+});
+
+gulp.task('default', ['sass', 'watch']);
