2095b19e4e93f6ec51dbd8d5d49e4b351d97e72c
Guikingone <guillaume.loulier@hotmail.fr>
2016-06-26 18:43:23 +0200

    [!C !F !-C] || Mise en place Sass|En cours + Integration maquette|En cours

--- src/AppBundle/Controller/Web/HomeController.php
+++ src/AppBundle/Controller/Web/HomeController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Web;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
@@ -11,9 +12,17 @@ class HomeController extends Controller
     /**
      * @Route("/", name="homepage")
      * @Method("GET")
+     * @Template("default/index.html.twig")
      */
-    public function indexAction()
+    public function indexAction(){}
+
+    /**
+     * @Route("/about", name="about")
+     * @Method("GET")
+     * @Template("default/about.html.twig")
+     */
+    public function aboutAction()
     {
-        return $this->render('default/index.html.twig');
+
     }
 }
