0561e0634bd1872cbcba5633377496d36e86b639
Guikingone <guillaume.loulier@hotmail.fr>
2016-09-20 09:22:37 +0200

    [+][!M] || Sass|In progress

---
+++ web/dev/Angular/Components/article.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "core-article",
+    templateUrl: "./web/Angular/Templates/articles.html.twig"
+})
+
+export class ArticleComponent {}
