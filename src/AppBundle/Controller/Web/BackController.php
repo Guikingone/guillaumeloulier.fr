4ac9cd01500a12a17b468b9a50f47870a95f0cd9
Guikingone <guillaume.loulier@hotmail.fr>
2016-09-23 18:07:48 +0200

    [+][!M] || Sass|In progress

--- src/AppBundle/Controller/Web/BackController.php
+++ src/AppBundle/Controller/Web/BackController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller\Web;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -14,8 +15,12 @@ class BackController extends Controller
 {
     /**
      * @Route("/", name="back_index")
+     * @Template("Back/index.html.twig")
+     *
+     * @return array
      */
     public function indexAction()
     {
+        return array('controller' => 'back');
     }
 }
