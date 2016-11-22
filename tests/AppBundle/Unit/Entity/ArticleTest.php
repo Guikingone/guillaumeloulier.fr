5ddac2e5e8a47a924596fb94d6ce5f297c831d74
Guikingone <guillaume.loulier@hotmail.fr>
2016-11-22 11:44:53 +0100

    [+][II][!M] || Tests|Corrections

---
+++ tests/AppBundle/Unit/Entity/ArticleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\AppBundle\Unit\Entity;
+
+class ArticleTest
+{
+    /**
+     * Test if the Entity can be hydrated.
+     */
+    public function testArticleBoot()
+    {
+
+    }
+}
