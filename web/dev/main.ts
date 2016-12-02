ea9a1c81f54ca0ba032cadce42329cf2c8baec9e
Guikingone <guillaume.loulier@hotmail.fr>
2016-12-02 19:47:47 +0100

    [+][II][!M][Angular] || Angular|Compiled

--- web/dev/main.ts
+++ web/dev/main.ts
@@ -1,4 +1,8 @@
-import './polyfills';
+import 'core-js';
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
--- web/dev/typescript/components/app.component.ts
+++ web/dev/typescript/components/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "core-introduction",
-    templateUrl: './../html/app.component.html',
-    styleUrls: ['./../html/app.component.css']
+    templateUrl: './dev/typescript/html/index.html',
+    styleUrls: ['./dev/typescript/html/index.css']
 })
 export class AppComponent {
 
