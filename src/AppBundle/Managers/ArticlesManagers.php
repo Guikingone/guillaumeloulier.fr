01130c4ccbf83e6965f5f89f133d9a4bfbb68561
Guikingone <guillaume.loulier@hotmail.fr>
2017-04-17 11:45:57 +0200

    [FIX](Core)[!M] || Insight|Fix on progress

---
+++ src/AppBundle/Managers/ArticlesManagers.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the guillaumeloulier.fr project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Managers;
+
+/**
+ * Class ArticlesManagers
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class ArticlesManagers
+{
+    private $doctrine;
+
+    private $form;
+}
